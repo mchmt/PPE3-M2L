49f8a7e03cb6e86bf40a72def284f33d1aed6376
mchmt <maureen.chaumat@gmail.com>
2013-01-04 03:55:00 -0800

    Create infoslegales.php

---
+++ infoslegales.php
@@ -0,0 +1,74 @@
+<h2 style="margin: 0; color: #1B618C;">Mentions légales</h2>
+<div class="nom">
+  Editeur
+</div>
+<div class="contenu">
+	Maison des Ligues de Lorraine
+	13 rue Jean Moulin - BP 70001
+	54510 TOMBLAINE
+	Tél. : 03.83.18.87.02
+	Fax : 03.83.18.87.03
+</div>
+
+<div class="nom">
+	Directeur de publication
+</div>
+<div class="contenu">
+	<i>Au sens de l'article 93-2 de la loi n° 82-652 du 29 juillet 1982</i>
+	Monsieur Lucien Sapin, Directeur de la Maison des Ligues de Lorraine
+</div>
+
+<div class="nom">
+	Prestataire d'hébergement
+</div>
+<div class="contenu">
+
+</div>
+
+<div class="nom">
+	Traitement des données à caractère personnel
+</div>
+<div class="contenu">
+	<a href="http://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000022443981">Délibération de la CNIL n° 2010-229 du 10 juin 2010 </a>
+	Dans cette délibération, la haute instance formule les observations suivantes : « Les traitements de données à caractère personnel mis en oeuvre par des organismes à but non lucratif pour la réalisation des seules finalités définies à l'article 2 et pour les seules données visées à l'article 3 comportant des données sur des personnes physiques constituent des traitements courants ne paraissant pas susceptibles de porter atteinte à la vie privée des personnes dans le cadre de leur utilisation régulière. »
+	L'article 2 de la délibération stipule que « les traitements doivent avoir pour seules finalités : l'enregistrement et la mise à jour des informations individuelles nécessaires à la gestion administrative des membres et donateurs, en particulier la gestion des cotisations, conformément aux dispositions statutaires qui régissent les intéressés ; d'établir, pour répondre à des besoins de gestion, des états statistiques ou des listes de membres ou de contacts, notamment en vue d'adresser bulletins, convocations, journaux ; d'établir des annuaires de membres, y compris lorsque ces annuaires sont mis à la disposition du public sur le réseau internet ; d'effectuer par tout moyen de communication des opérations relatives à des actions de prospection auprès des membres, donateurs et prospects. »
+	L’article 3 énonce les données traitées pour la réalisation des finalités décrites à l'article 2 :
+	- l'identité : nom, prénoms, sexe, date de naissance, adresse, numéros de téléphone (fixe et mobile) et de télécopie, adresse de courrier électronique ;
+	- les informations relatives à la gestion administrative de l'organisme : état des cotisations, position vis-à-vis de l'association, informations strictement liées à l'objet statutaire de l'organisme, identité bancaire pour la gestion des dons ;
+	- données de connexion (date, heure, adresse internet protocole de l'ordinateur du visiteur, page consultée) à des seules fins statistiques d'estimation de la fréquentation du site.
+	Le même article précise également les données qui ne peuvent pas bénéficier de l'exonération de déclaration à la CNIL : les données qui font apparaître, directement ou indirectement, les origines raciales ou ethniques, les opinions politiques, philosophiques ou religieuses ou l'appartenance syndicale des personnes, ou qui sont relatives à la santé ou à la vie sexuelle de celles-ci (art. 8 de la loi du 6 janvier 1978 modifiée) ; les données concernant les infractions, condamnations ou mesures de sûreté (art. 9 de la loi du 6 janvier 1978 modifiée) ; les données relatives aux difficultés sociales et économiques des personnes ; le numéro d'inscription au répertoire d'identification des personnes (numéro INSEE ou numéro de sécurité sociale).
+</div>
+
+<div class="nom">
+	Prestataire de conception graphique
+</div>
+<div class="contenu">
+	La conception et les développements graphiques et ergonomiques ainsi que le développement des modèles de pages ont été réalisés par la société S.L.A.M. en 2012.
+</div>
+
+<div class="nom">
+	Liens hypertextes (responsabilité)
+</div>
+<div class="contenu">
+	<b>m2l.fr</b> propose de nombreux liens vers d’autres sites, essentiellement des sites officiels (institutions, organismes publics, etc.). Nous indiquons systématiquement vers quel site nous vous proposons d’aller. Cependant, ces pages web dont les adresses sont régulièrement vérifiées ne font pas partie du portail : elles n’engagent donc pas la responsabilité de la rédaction du site.
+</div>
+
+<div class="nom">
+	Droit de reproduction
+</div>
+<div class="contenu">
+	La reproduction ou réutilisation des contenus des pages de m2l.fr est autorisée sous réserve de mentionner la date et la source. La copie du logo doit avoir obtenu l’autorisation du directeur de la publication. La création de liens est possible dans les conditions fixées ci-après.
+</div>
+
+<div class="nom">
+	Etablir un lien
+</div>
+<div class="contenu">
+	Tout site public ou privé est autorisé à établir, sans autorisation préalable, un lien vers les informations diffusées sur m2l.fr. En revanche, les pages du portail ne doivent pas être imbriquées à l’intérieur des pages d’un autre site.
+</div>
+
+<hr />
+<br />
+
+
+
