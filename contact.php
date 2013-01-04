e5e63c6f1cc2c845c19e6c1ead92782b9db1916f
mchmt <maureen.chaumat@gmail.com>
2013-01-04 12:53:10 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,63 +1,49 @@
 <h2 style="margin: 0; color: #1B618C;">Contact</h2>
 <div class="headerPage">
-	Prendre contact avec la Maison des Ligues
+L'administration de la Maison des Ligues est à votre disposition pour toute information.
 </div>
-<div class="nom">
-	Maison des Ligues de Lorraine
-</div>
-<div class="chepas">
-	13 rue Jean Moulin - BP 70001
-	54510 TOMBLAINE
-	Tél. : 03.83.18.87.02
-	Fax : 03.83.18.87.03
-	Email  : contact@m2l.fr
-</div>
-
-<div class="nom">
-	Directeur - Lucien SAPIN
-</div>
-<div class="chepas">
-	Tél. : 03 83 18 87 02
-	Fax : 03 83 18 87 03
-	Email : lucien.gastaldello@m2l.fr
-</div>
-
-<div class="nom">
-	Secrétaire de direction - Martine NEBOUT
-</div>
-<div class="chepas">
-	Tél. : 03 83 18 87 02
-	Email : martine.bernard@m2l.fr
-</div>
-
-<div class="nom">
-	Chargée Impact Emploi - Nathalie GENAIS
-</div>
-<div class="chepas">
-	Tél. : 03 83 18 87 05
-	Email : martine.genoux@m2l.fr
-</div>
-
-<div class="nom">
-	Chargée d’infographie - Lorette GIROUX
-</div>
-<div class="chepas">
-	Tél. : 03 83 18 87 06
-	Email : lorette.bossart@m2l.fr
-</div>
-
-<div class="nom">
-	Secrétaire Comptable - Jeannine BEN ALI
-</div>
-<div class="chepas">
-	Tél. : 03 83 18 87 07
-	Fax : 03 83 18 87 03
-	Email : jeannine.vuillemard@m2l.fr
-	HAHAHAHAHAHAH
-</div>
-
 <hr />
 <br />
+<div class="nom">Maison des Ligues de Lorraine<br /></div>
+<div class="coordonnees">
+13 rue Jean Moulin - BP 70001<br />
+54510 TOMBLAINE<br />
+Tél. : 03.83.18.87.02<br />
+Fax : 03.83.18.87.03<br />
+Email : contact@m2l.fr<br /><br />
+</div>
+<div class="intitule">Directeur</div>
+<div class="nom">Lucien SAPIN<br /></div>
+<div class="coordonnees">
+Tél. : 03 83 18 87 02<br />
+Fax : 03 83 18 87 03<br />
+Email : lucien.gastaldello@m2l.fr<br /><br />
+</div>
+<div class="intitule">Secrétaire de direction</div>
+<div class="nom">Martine NEBOUT<br /></div>
+<div class="coordonnees">
+Tél. : 03 83 18 87 02<br />
+Email : martine.bernard@m2l.fr<br /><br />
+</div>
+<div class="intitule">Chargée Impact Emploi</div>
+<div class="nom">Nathalie GENAIS<br /></div>
+<div class="coordonnees">
+Tél. : 03 83 18 87 05<br />
+Email : martine.genoux@m2l.fr<br /><br />
+</div>
+<div class="intitule">Chargée d’infographie</div>
+<div class="nom">Lorette GIROUX<br /></div>
+<div class="coordonnees">
+Tél. : 03 83 18 87 06<br />
+Email : lorette.bossart@m2l.fr<br /><br />
+</div>
+<div class="intitule">Secrétaire Comptable </div>
+<div class="nom">Jeannine BEN ALI<br /></div>
+<div class="coordonnees">
+Tél. : 03 83 18 87 07<br />
+Fax : 03 83 18 87 03<br />
+Email : jeannine.vuillemard@m2l.fr<br /><br />
+</div>
 
-
+<br />
 
