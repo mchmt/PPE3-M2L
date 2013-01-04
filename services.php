33daad347fca5a1cf2bc7b826c846fa95300073d
mchmt <maureen.chaumat@gmail.com>
2013-01-04 03:55:25 -0800

    Create services.php

---
+++ services.php
@@ -0,0 +1,100 @@
+<h2 style="margin: 0; color: #1B618C;">Services</h2>
+<div class="headerPage">
+  Ensemble des services à disposition des ligues
+</div>
+<hr />
+<br />
+<div class="nomService">Accès internet<br /></div>
+<div class="descriptService">
+	Les ligues disposent d'un accès Internet mutualisé que la M2L loue à un prestataire extérieur.
+	<br />
+</div>
+<div class="nomService">Accès WIFI<br /></div>
+<div class="descriptService">
+	Dans tous les espaces, un réseau Wifi "visiteurs" est disponible, avec une clé WPA renouvelée régulièrement et communiquée aux ligues. Ce réseau ne permet que l'accès à Internet.
+	<br />
+</div>
+<div class="nomService">Téléphonie<br /></div>
+<div class="descriptService">
+	Dans les bâtiments anciens A et B, les salles et bureaux sont équipés de prises de téléphone analogiques. La M2L y fournit les combinés téléphoniques. Dans les bâtiments neufs C et D, l'équipement téléphonique est de type VoIP. La M2L loue des postes téléphoniques IP aux ligues.
+	<br />
+</div>
+<div class="nomService">Affranchissement<br /></div>
+<div class="descriptService">
+	Une machine à affranchir permet un affranchissement rapide et en nombre. Cette prestation est facturée aux ligues au coût de l'affranchissement. Chaque mois, on relie la machine à affranchir à une imprimante pour obtenir une liste de codes de gestion correspondant aux ligues associés à une quantité et un type d'affranchissement. La prise en compte de ces informations permet au CROSL d'éditer des factures.
+	<br />
+</div>
+<div class="nomService">Impressions<br /></div>
+<div class="descriptService">
+	Les ligues disposent de la possibilité d'imprimer en volume et en qualité d'imprimerie sur des ressources d'impression numériques connectées situées dans le local reprographie du rez-de-chaussée dont l'usage fait l'objet d'une facturation à prix coûtant.<br />
+	Un système de comptage situé sur le serveur d'impression permet au CROSL d’effectuer une facturation mensuelle auprès des ligues :
+    <ul>
+		<li>une photocopieuse noir et blanc à 70 pages/minute avec différents dispositifs de finition ;</li>
+		<li>une imprimante Laser couleur A4/A3 à encre solide à 25 pages/minute ;</li>
+		<li>un traceur A2 (1 page / minute) utilisé pour les affiches et banderoles.</li>
+	</ul>
+	<br />
+</div>
+<div class="nomService">Serveur FTP documentaire<br /></div>
+<div class="descriptService">
+	La M2L met à disposition des ligues un serveur FTP documentaire intranet/internet regroupant des textes légaux, des modèles de dossiers, de statuts, des programmes de formation (...) compilés par le CROSL.
+	<br />
+</div>
+<div class="nomService">Digicode du jour et clé WIFI<br /></div>
+<div class="descriptService">
+	La M2L met à disposition des ligues un site web d'information sur le digicode permettant l'accès à la M2L ainsi que sur la clé Wifi "visiteurs". Le système de réservation donne également le digicode du jour dans le compte-rendu de réservation envoyé automatiquement par mail.
+	<br />
+</div>
+<div class="nomService">Système de gestion des configurations<br /></div>
+<div class="descriptService">
+	M2L gère à travers un logiciel de gestion des configurations l'ensemble du parc informatique incluant les postes fixes des ligues.
+	<br />
+</div>
+<div class="nomService">Intégration des postes informatiques des ligues<br /></div>
+<div class="descriptService">
+	<ul class="disc">Lorsque les ligues acquièrent du matériel informatique, il y a une phase obligatoire d'intégration qui consiste à :
+		<li>installer un antivirus affilié au serveur antiviral de la M2L ;</li>
+		<li>installer la dernière version de l'agent qui réalise l’inventaire matériel et logiciel ;</li>
+		<li>paramétrer le poste en adressage IP automatique ;</li>
+		<li>installer un système de sauvegarde de données sur un site FTP de sauvegarde géré par la M2L ;</li>
+		<li>effectuer les dernières mises à jour systèmes et à paramétrer leur automatisation ;</li>
+		<li>paramétrer les noms des postes selon les règles de gestion suivantes :
+		<ul class="cercle">
+			<li>B[code bâtiment]E[numéro étage]L[numéro ligue]S[numéro salle].P[numéro poste]</li>
+			<li>Code bâtiment qui peut être A ou C</li>
+			<li>N° étage est compris entre 1 et 4 (puisque les locaux du rez-de-chaussée n'hébergent pas de ligues)</li>
+			<li>N° ligue sur 2 chiffres : correspond à un nombre attribué à la ligue allant pour l'instant de 01 à 24</li>
+			<li>N° salle sur 2 chiffres : correspond aux bureaux occupés par les ligues</li>
+			<li>N° poste sur 2 chiffres : correspond au numéro écrit sur la prise murale</li>
+			<i>Exemple : le nom d'hôte BAE2L06S01P01 correspond au poste installé sur la prise N°1 du bureau A201occupé par la ligue de Volley, bureau situé au deuxième étage du bâtiment A.</i>
+		</ul>
+		</li>
+	</ul>
+	Cette intégration est contractualisée. Les ligues et CD étant toutes des structures associatives indépendantes, leurs postes ne sont pas intégrés dans un annuaire central. Par contre, les postes de l'administration de la M2L et de la salle multimédia le sont.
+	<br />
+</div>
+<div class="nomService">Intégration d'imprimantes<br /></div>
+<div class="descriptService">
+	Lorsque les structures hébergées s'équipent d'imprimante réseau, la connexion en est réalisée par l'informaticien bénévole du CROSL, sans passer par un serveur d'impression. Le nom de l'imprimante est lui aussi codifié de la même façon que celui des postes (la lettre L vient remplacer la lettre P).
+	<br />
+</div>
+<div class="nomService">Réservation de salles<br /></div>
+<div class="descriptService">
+	La M2L met à disposition des ligues un site web de réservation des salles (réunions, amphithéâtre, restauration). Ce site est accessible en intranet, mais aussi depuis l'internet. Les réservations payantes sont facturées par la Région aux utilisateurs. L’administration de la M2L lui communique les informations nécessaires à cette facturation de façon hebdomadaire.
+	<br />
+</div>
+<div class="nomService">Etablissement de bulletins de salaire<br /></div>
+<div class="descriptService">
+	Le CRIB (Centre Régional d'Information des Bénévoles) est un label donné au CROSL qui, entre autres missions d'information (sur les textes réglementaires, la convention nationale du sport ...), propose un service d'établissement de bulletins de salaires aux ligues et à leurs clubs affiliés. Le CROSL est tiers de confiance pour l'URSSAF et, à ce titre, établit des bulletins de salaires réglementaires et tous les documents annexes. La prestation est facturée au forfait (60 € par an) et au bulletin édité (5 €). Un employé du CROSL est affecté à cette mission à raison de 0,8 ETP.
+	<br />
+</div>
+<div class="nomService">Formations</div>
+<div class="descriptService">
+	Le CROSL offre un catalogue diversifié de formations aux bénévoles des clubs affiliés aux ligues (législation, éthique, comptabilité associative, etc.). Les ligues organisent également des formations, en général plus techniques, sur l'usage de logiciels spécifiques de gestion des clubs ou des compétitions sportives.
+	<br />
+</div>
+
+<br />
+
+
+
