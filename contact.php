b660a896723bc9a20ac3ba334acacde955ad280e
mchmt <maureen.chaumat@gmail.com>
2013-01-10 10:29:54 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,43 +6,43 @@ L'administration de la Maison des Ligues est à votre disposition pour toute inf
 <br />
 <div class="nom">Maison des Ligues de Lorraine<br /></div>
 <div class="coordonnees">
-13 rue Jean Moulin - BP 70001<br />
-54510 TOMBLAINE<br />
-Tél. : 03.83.18.87.02<br />
-Fax : 03.83.18.87.03<br />
-Email : contact@m2l.fr<br /><br />
+    13 rue Jean Moulin - BP 70001<br />
+    54510 TOMBLAINE<br />
+    Tél. : 03.83.18.87.02<br />
+    Fax : 03.83.18.87.03<br />
+    Email : contact@m2l.fr<br /><br />
 </div>
 <div class="intitule">Directeur</div>
 <div class="nom">Lucien SAPIN<br /></div>
 <div class="coordonnees">
-Tél. : 03 83 18 87 02<br />
-Fax : 03 83 18 87 03<br />
-Email : lucien.gastaldello@m2l.fr<br /><br />
+    Tél. : 03 83 18 87 02<br />
+    Fax : 03 83 18 87 03<br />
+    Email : lucien.gastaldello@m2l.fr<br /><br />
 </div>
 <div class="intitule">Secrétaire de direction</div>
 <div class="nom">Martine NEBOUT<br /></div>
 <div class="coordonnees">
-Tél. : 03 83 18 87 02<br />
-Email : martine.bernard@m2l.fr<br /><br />
+    Tél. : 03 83 18 87 02<br />
+    Email : martine.bernard@m2l.fr<br /><br />
 </div>
 <div class="intitule">Chargée Impact Emploi</div>
 <div class="nom">Nathalie GENAIS<br /></div>
 <div class="coordonnees">
-Tél. : 03 83 18 87 05<br />
-Email : martine.genoux@m2l.fr<br /><br />
+    Tél. : 03 83 18 87 05<br />
+    Email : martine.genoux@m2l.fr<br /><br />
 </div>
 <div class="intitule">Chargée d’infographie</div>
 <div class="nom">Lorette GIROUX<br /></div>
 <div class="coordonnees">
-Tél. : 03 83 18 87 06<br />
-Email : lorette.bossart@m2l.fr<br /><br />
+    Tél. : 03 83 18 87 06<br />
+    Email : lorette.bossart@m2l.fr<br /><br />
 </div>
 <div class="intitule">Secrétaire Comptable </div>
 <div class="nom">Jeannine BEN ALI<br /></div>
 <div class="coordonnees">
-Tél. : 03 83 18 87 07<br />
-Fax : 03 83 18 87 03<br />
-Email : jeannine.vuillemard@m2l.fr<br /><br />
+    Tél. : 03 83 18 87 07<br />
+    Fax : 03 83 18 87 03<br />
+    Email : jeannine.vuillemard@m2l.fr<br /><br />
 </div>
 
 <br />
