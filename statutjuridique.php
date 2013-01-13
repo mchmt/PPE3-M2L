2151c4ef0c0de8790338c1860239b91fc29daad6
mchmt <maureen.chaumat@gmail.com>
2013-01-13 09:44:09 -0800

    Create statutjuridique.php

---
+++ statutjuridique.php
@@ -0,0 +1,15 @@
+<h2 style="margin: 0; color: #1B618C;">Statut juridique</h2>
+<div class="headerPage">
+Statut juridique et financement de la M2L
+</div>
+<hr />
+<br />
+<div class="reponse">
+  La Maison des Ligues est un établissement du Conseil Régional. Ce n’est pas une entité juridique en propre.<br />
+  Elle est financée à 100 % (pour son fonctionnement et pour la construction récente de l’extension des
+  bâtiments C et D) par le Conseil Régional et sans aucune participation du Conseil Général de Meurthe et
+  Moselle, bien qu’elle abrite un certain nombre de comités départementaux. Une convention de cogestion
+  a été passée entre le Conseil Régional et le Comité Régional Olympique et Sportif de Lorraine pour la
+  gestion de l’outil « Maison des Ligues ». <br />
+  Le CROSL est une association financée par le ministère via le CNDS (Centre National de Développement du Sport)
+</div>
